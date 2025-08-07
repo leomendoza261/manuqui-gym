f780fb1424f07e7c2070a0dcc687d069a06cce1a
leomendoza261 <pedrolmendoza031297@gmail.com>
2025-08-07 16:33:45 -0300

    correcion en el nommbre

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -56,7 +56,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav rol={rol} />
-            <h1 className='font-bold text-xl'>Manuqui Gym</h1>
+            <h1 className='font-bold text-xl'>Creybit Gym</h1>
             {/* <SearchInput />
             <User /> */}
           </header>
